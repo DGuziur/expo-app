8e80e3e533f47c09abc9b575bdf85e2e4782fb4c
DGuziur <dondxd@o2.pl>
2025-08-17 00:22:14 +0200

    feat: set up firebase and basic auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
+        headerRight: () => (
+          <TouchableOpacity onPress={() => router.replace("/login")}>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ marginRight: 10 }}>{"User"}</Text>
+              <Image
+                source={{ uri: "https://i.pravatar.cc/40" }}
+                style={{
+                  width: 32,
+                  height: 32,
+                  borderRadius: 16,
+                  marginRight: 50,
+                }}
+              ></Image>
+            </View>
+          </TouchableOpacity>
+        ),
         headerStyle: {
           backgroundColor: "powderblue",
         },
@@ -16,8 +40,12 @@ export default function RootLayout() {
     >
       <Stack.Screen
         name="index"
-        options={{ title: "Units", headerTitleAlign: "center" }}
+        options={{
+          title: "Units",
+          headerTitleAlign: "center",
+        }}
       />
+      <Stack.Screen name="login/index" options={{ headerShown: false }} />
     </Stack>
   );
 }
---
+++ firebaseInit.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+import { Platform } from "react-native";
+
+function getRequiredEnvVar(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing required environment variable: ${name}`);
+  }
+  return value;
+}
+
+const firebaseConfig = {
+  apiKey: getRequiredEnvVar("EXPO_PUBLIC_API_KEY"),
+  authDomain: getRequiredEnvVar("EXPO_PUBLIC_AUTH_DOMAIN"),
+  projectId: getRequiredEnvVar("EXPO_PUBLIC_PROJECT_ID"),
+  storageBucket: getRequiredEnvVar("EXPO_PUBLIC_STORAGE_BUCKET"),
+  messagingSenderId: getRequiredEnvVar("EXPO_PUBLIC_MESSAGING_SENDER_ID"),
+  appId: getRequiredEnvVar("EXPO_PUBLIC_ID"),
+  measurementId: getRequiredEnvVar("EXPO_PUBLIC_MEASUREMENT_ID"),
+};
+
+const app = initializeApp(firebaseConfig);
+if (Platform.OS === "web") {
+  //   initializeAnalytics(app);
+}
+initializeAuth(app);
+
+export default app;

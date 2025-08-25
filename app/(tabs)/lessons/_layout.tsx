8bacc37fde30315f9a86e1b10163257deee2359c
Falalatina <karolina40.gorska17@gmail.com>
2025-08-25 23:36:29 +0200

    fix/ layout router for forms

--- app/(tabs)/lessons/_layout.tsx
+++ app/(tabs)/lessons/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function LessonsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="form/FormLesson" options={{ title: "Add Lesson" }} />
+      <Stack.Screen name="forms/FormLesson" options={{ title: "Add Lesson" }} />
     </Stack>
   );
 }

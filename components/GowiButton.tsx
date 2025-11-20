72d97ce4aed2de66456801747fe907da41773266
DGuziur <dondxd@o2.pl>
2025-11-20 17:08:21 +0100

    fix: GowiButton margin on android

--- components/GowiButton.tsx
+++ components/GowiButton.tsx
@@ -1,7 +1,7 @@
 import { buttonsStyles } from "@/assets/styles/buttons";
 import { themeColors } from "@/themes/themeColors";
 import { LinearGradient } from "expo-linear-gradient";
-import { Pressable, Text, ViewStyle } from "react-native";
+import { ColorValue, Pressable, Text, View, ViewStyle } from "react-native";
 
 type GowiButtonProps = {
   title: string;
@@ -15,26 +15,32 @@ type GowiButtonProps = {
 
 const variants = {
   primary: {
-   // colors: ["#FFE044", "#FF8C42", "#CA009E"],
-    colors: themeColors.gradientsDarkMOde.cta,
-    pressedColors: ["#FFA64A"],
+    colors: themeColors.gradientsDarkMOde.cta as [
+      ColorValue,
+      ColorValue,
+      ColorValue
+    ],
+    pressedColors: ["#FFA64A", "#FFA64A"] as [ColorValue, ColorValue],
     borderColor: "#B34602",
     textColor: "#FFFFFF",
-    hasShadow: true,
   },
   secondary: {
-    colors: [themeColors.secondary[700]],
-    pressedColors: [themeColors.secondary[300]],
+    colors: [themeColors.secondary[700], themeColors.secondary[700]] as [
+      ColorValue,
+      ColorValue
+    ],
+    pressedColors: [themeColors.secondary[300], themeColors.secondary[300]] as [
+      ColorValue,
+      ColorValue
+    ],
     borderColor: "#6A2AAB",
     textColor: "#FFFFFF",
-    hasShadow: false,
   },
   disabled: {
-    colors: ["#666666"],
-    pressedColors: ["#404040"],
+    colors: ["#666666", "#666666"] as [ColorValue, ColorValue],
+    pressedColors: ["#404040", "#404040"] as [ColorValue, ColorValue],
     borderColor: "#8A8A8A",
     textColor: "#B3B3B3",
-    hasShadow: false,
   },
 };
 
@@ -50,47 +56,45 @@ export default function GowiButton({
   const config = variants[type];
 
   const sizeStyle = square
-      ? size === "L"
-        ? {
-            width: 56,
-            height: 56,
-            borderRadius: 12,
-            alignItems: "center",
-            justifyContent: "center",
-          } as ViewStyle
-        : {
-            width: 40,
-            height: 40,
-            borderRadius: 8,
-            alignItems: "center",
-            justifyContent: "center",
-          } as ViewStyle
-      : size === "L"
-      ? buttonsStyles.base_L
-      : buttonsStyles.base_S;
+    ? size === "L"
+      ? ({
+          width: 56,
+          height: 56,
+          borderRadius: 12,
+          alignItems: "center",
+          justifyContent: "center",
+        } as ViewStyle)
+      : ({
+          width: 40,
+          height: 40,
+          borderRadius: 8,
+          alignItems: "center",
+          justifyContent: "center",
+        } as ViewStyle)
+    : size === "L"
+    ? buttonsStyles.base_L
+    : buttonsStyles.base_S;
 
   if (textOnly) {
     return (
       <Pressable onPress={onPress}>
         {({ pressed }) => (
           <LinearGradient
-            colors={pressed ? ["rgba(255, 255, 255, 0.25)"] : ["transparent"]}
+            colors={
+              pressed
+                ? ["rgba(255,255,255,0.25)", "rgba(255,255,255,0.25)"]
+                : ["transparent", "transparent"]
+            }
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
-            style={[
-              sizeStyle,
-              {
-                borderBottomColor: "transparent",
-                borderBottomWidth: 0,
-              },
-            ]}
+            style={[sizeStyle]}
           >
             <Text
-              style={[
-                buttonsStyles.text,
-                { color: type === "disabled" ? "#666666" : "#FFFFFF" },
-                { textDecorationLine: underline ? "underline" : "none" },
-              ]}
+              style={{
+                ...buttonsStyles.text,
+                color: type === "disabled" ? "#666" : "#fff",
+                textDecorationLine: underline ? "underline" : "none",
+              }}
             >
               {title} &#8594;
             </Text>
@@ -101,32 +105,42 @@ export default function GowiButton({
   }
 
   return (
-    <Pressable onPress={onPress} > 
-      {({ pressed }) => (
-        <LinearGradient
-          colors={pressed ? config.pressedColors : config.colors}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-          style={[
-            sizeStyle,
-            config.hasShadow && buttonsStyles.shadow,
-            {
-              borderBottomColor: config.borderColor,
-              borderBottomWidth: square ? 5 : 3,
-            },
-          ]}
-        >
-          <Text
-            style={[
-              buttonsStyles.text,
-              { color: config.textColor },
-              { textDecorationLine: underline ? "underline" : "none" },
-            ]}
+    <View style={{ position: "relative" }}>
+      <View
+        style={{
+          ...sizeStyle,
+          position: "absolute",
+          top: 4,
+          width: "100%",
+          backgroundColor: config.borderColor,
+          borderRadius: sizeStyle.borderRadius,
+        }}
+      />
+
+      <Pressable onPress={onPress}>
+        {({ pressed }) => (
+          <LinearGradient
+            colors={pressed ? config.pressedColors : config.colors}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={{
+              ...sizeStyle,
+              borderRadius: sizeStyle.borderRadius,
+              transform: [{ translateY: pressed ? 4 : 0 }],
+            }}
           >
-            {title} &#8594;
-          </Text>
-        </LinearGradient>
-      )}
-    </Pressable>
+            <Text
+              style={{
+                ...buttonsStyles.text,
+                color: config.textColor,
+                textDecorationLine: underline ? "underline" : "none",
+              }}
+            >
+              {title} &#8594;
+            </Text>
+          </LinearGradient>
+        )}
+      </Pressable>
+    </View>
   );
 }
